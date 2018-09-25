39d4ed6752081d6e8d235e5f1138f542f9ab8a10
francescodc87 <francescodc87@gmail.com>
2018-09-25 10:53:46 +0100

    New version

--- ui.R
+++ ui.R
@@ -1,15 +1,19 @@
-
-
 library(markdown)
 # loading data I need
-load("data/modules_explorer_data.Rdata")
+#load("data/modules_explorer_data.Rdata")
+nomi <- c("MIB score","Shannon's Entropy","Module length","N clusters in",
+          "N clusters MiBiG in","max.pval","N compound classes","compound classes",
+          "CORE","CORE/TAILORING","MIXED","OTHER","REGULATOR","REGULATOR/TAILORING",
+          "TAILORING","TAILORING/CORE","TAILORING/REGULATOR","TAILORING/TRANSPORT",
+          "TRANSPORT")
 
 navbarPage("Modules Explorer",
-           tabPanel("Summary Tables and MIB score",
+           #tabPanel("Summary Tables and MIB score",
                     sidebarLayout(
                       sidebarPanel(
-                        actionButton("do", "UPDATE"),
-                        actionButton("select", "Select Module"),
+                        actionButton("do", "UPDATE MIB score"),
+                        #div(style="display:inline-block; width: 100px",checkboxInput("redundancy", "top20", value = FALSE, width = NULL)),
+                        uiOutput("tab"),
                         div(style = "height:10px;", " "),
                         div(style="display:inline-block; width: 100px",numericInput("W1", "# compounds", 0, min=-15,max=15)),
                         div(style="display:inline-block; width: 100px",numericInput("W2", "Shannon's Entropy", 15, min=-15,max=15)),
@@ -30,29 +34,20 @@ navbarPage("Modules Explorer",
                         div(style="display:inline-block; width: 100px",numericInput("MIBIG", "min hits in MiBiG", 1, min=0,max=100)),
                         checkboxGroupInput('show_vars',
                                            'Columns in Metrics and Ranks',
-                                           colnames(Metrics.Modules.shiny),
-                                           selected = colnames(Metrics.Modules.shiny)[1:7], inline = TRUE)
+                                           nomi,
+                                           selected = nomi[1:7], inline = TRUE),
+                        div(style = "height:10px;", " "),
+                        div(style = "height:10px;", " "),
+                        div(style = "height:10px;", " "),
+                        tags$img(src="logos.png", height=80)
                       ),
                       mainPanel(
                         tabsetPanel(
                           tabPanel('Modules metrics',
                                    dataTableOutput("mytable1")),
-                          tabPanel('top 20 modules - excluding related ones',
-                                   dataTableOutput("mytable2"))#,
-                          #tabPanel('Module composition',
-                          #         dataTableOutput("mytable3"))
+                          tabPanel('Module smCOG composition', dataTableOutput("mytable2"))
                         )
                       )
-                    )
-        
-        ),
-           tabPanel("Clusters",
-                    fluidRow(
-                      column(width=12, dataTableOutput("clust.hitted"))
-                    )
                   )
-           
-           
-        
-)
+              )
 
